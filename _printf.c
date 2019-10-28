aeafee1f6d9a2a0a38035930a75148295d4b2be4
MatriMariem <meriemmatri1994@gmail.com>
2019-10-28 04:47:17 -0700

    optimized code

--- _printf.c
+++ _printf.c
@@ -2,46 +2,48 @@
 #include <stdarg.h>
 #include "holberton.h"
 /**
- * _printf - prints anything
- * @format: pointer to string that contains specifiers
- * Return: number of characters printed
- */
+* _printf - prints anything
+* @format: pointer to string that contains specifiers
+* Return: number of characters printed
+**/
 int _printf(const char *format, ...)
 {
-int count = 0, i = 0;
+unsigned int count = 0, i = 0;
 int (*f)(va_list);
 va_list list;
-va_start(list, format);
+
 if (format == '\0')
-return (-1); /*idk*/
-while (format[i])
+return (-1);
+va_start(list, format);
+while (format && format[i])
 {
-for (; (format[i] != '\0' && format[i] != '%'); i++)
+if (format[i] != '%')
 {
 _putchar(format[i]);
 count++;
 }
-if (format[i] == '\0')
-return (count);
 else if (format[i] == '%' && format[i + 1] == '\0')
 return (-1);
+else if (format[i] == '\0')
+return (count);
 else if (format[i] == '%')
 {
 f = getspecifier(format[i + 1]);
-i += 2;
+i += 1;
 if (f == '\0')
 {
-if (format[i - 1] != '%')
+if (format[i] != '%')
 {
-_putchar('%');
+_putchar(format[i - 1]);
 count += 1;
 }
-_putchar(format[i - 1]);
+_putchar(format[i]);
 count += 1;
 }
 else
 count = count + f(list);
 }
+i++;
 }
 va_end(list);
 return (count);
